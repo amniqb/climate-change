5ac4661ab4a283c640efd7b25d7e9fbc2d8ad0a8
Amna Iqbal <2000amnaiqbal@gmail.com>
2025-08-30 16:48:36 +0200

    lazy / suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import Home from "./pages/Home/Home";
-import Temperature from "./pages/Temperature/Temperature";
-import CO2 from "./pages/CO2/CO2";
-import Methane from "./pages/Methane/Methane";
-import NitrousOxide from "./pages/NitrousOxide/NitrousOxide";
-import Arctic from "./pages/Arctic/Arctic";
+import Loader from "./components/Loader";
+import { Suspense, lazy } from "react";
 
-export default function App(){
-  return ( 
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/temperature" element={<Temperature />} />
-        <Route path="/co2" element={<CO2 />} />
-        <Route path="/methane" element={<Methane />} />
-        <Route path="/nitrous-oxide" element={<NitrousOxide />} />
-        <Route path="/arctic" element={<Arctic />} />
-      </Route>
-    </Routes>
+
+const Home = lazy(() => import("./pages/Home/Home"));
+const Temperature = lazy(() => import("./pages/Temperature/Temperature"));
+const CO2 = lazy(() => import("./pages/CO2/CO2"));
+const Methane = lazy(() => import("./pages/Methane/Methane"));
+const NitrousOxide = lazy(() => import("./pages/NitrousOxide/NitrousOxide"));
+const Arctic = lazy(() => import("./pages/Arctic/Arctic"));
+
+export default function App() {
+  return (
+    <Suspense fallback={<Loader label="Loading page..." />}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/temperature" element={<Temperature />} />
+          <Route path="/co2" element={<CO2 />} />
+          <Route path="/methane" element={<Methane />} />
+          <Route path="/nitrous-oxide" element={<NitrousOxide />} />
+          <Route path="/arctic" element={<Arctic />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
